2735a12c6136895d29eac7ccf1251316662776f4
Nathan Morton <nathan.j.morton@gmail.com>
2020-05-28 21:03:41 -0700

    this

--- src/app.js
+++ src/app.js
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-ReactDOM.render(<p>This is my test boilerplate</p>, document.querySelector('#app'))
+ReactDOM.render(<p>This is my test boilerplate test</p>, document.querySelector('#app'))
+
